c2758fb51275c3102bbb220e30554a51cfd860c7
“adil62” <adilismail62@gmail.com>
2021-02-11 23:04:45 +0530

    Signup + login UI complete.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "./pages/Auth";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import CreateEvent from "./pages/CreateEvent";
 import Home from "./pages/Home";
 
@@ -9,7 +10,9 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Auth} />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
         <Route path="/home" component={Home} />
         <Route path="/create-event" component={CreateEvent} />
       </Switch>
--- src/pages/Auth/index.js
+++
@@ -1,7 +0,0 @@
-import Signup from "./components/Signup";
-
-const Auth = () => {
-  return <Signup />;
-};
-
-export default Auth;
